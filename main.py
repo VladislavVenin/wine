765fcac4364f1d52191c48c8651ddb717eb38d7d
VladislavVenin <d4spda@yandex.ru>
2025-11-15 07:38:36 +1100

    upload

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+import pandas
+import collections
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+now = datetime.datetime.now()
+founded_date = 1920
+age = now.year - founded_date
+
+age_last = int(str(age)[-2:])
+age_suffix = "лет"
+if age_last == 1 or str(age)[-1] == '1':
+    age_suffix = "год"
+if age_last in [2, 3, 4] or str(age)[-1] in ['2', '3', '4']:
+    age_suffix = "года"
+if age_last > 10 and age_last <= 20:
+    age_suffix = "лет"
+
+wines_df = pandas.read_excel('wine3.xlsx',
+                             sheet_name='Лист1',
+                             keep_default_na="").to_dict(orient='records')
+
+wines = collections.defaultdict(list)
+for wine in wines_df:
+    category = wine['Категория']
+    wines[category].append(wine)
+
+rendered_page = template.render(
+    age=age,
+    age_suffix=age_suffix,
+    wines=wines,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
