6d9abdb2787b1df46c0b6e5929813e3b23379d2c
VladislavVenin <d4spda@yandex.ru>
2025-11-22 10:15:33 +1100

    ifmain / def main

--- main.py
+++ main.py
@@ -4,43 +4,49 @@ import pandas
 import collections
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-now = datetime.datetime.now()
-founded_date = 1920
-age = now.year - founded_date
+    template = env.get_template('template.html')
 
-age_last = int(str(age)[-2:])
-age_suffix = "лет"
-if age_last == 1 or str(age)[-1] == '1':
-    age_suffix = "год"
-if age_last in [2, 3, 4] or str(age)[-1] in ['2', '3', '4']:
-    age_suffix = "года"
-if age_last > 10 and age_last <= 20:
+    now = datetime.datetime.now()
+    founded_date = 1920
+    age = now.year - founded_date
+
+    age_last = int(str(age)[-2:])
     age_suffix = "лет"
+    if age_last == 1 or str(age)[-1] == '1':
+        age_suffix = "год"
+    if age_last in [2, 3, 4] or str(age)[-1] in ['2', '3', '4']:
+        age_suffix = "года"
+    if age_last > 10 and age_last <= 20:
+        age_suffix = "лет"
 
-wines_df = pandas.read_excel('wines.xlsx',
-                             sheet_name='Лист1',
-                             keep_default_na="").to_dict(orient='records')
+    wines_df = pandas.read_excel('wines.xlsx',
+                                 sheet_name='Лист1',
+                                 keep_default_na="").to_dict(orient='records')
 
-wines = collections.defaultdict(list)
-for wine in wines_df:
-    category = wine['Категория']
-    wines[category].append(wine)
+    wines = collections.defaultdict(list)
+    for wine in wines_df:
+        category = wine['Категория']
+        wines[category].append(wine)
 
-rendered_page = template.render(
-    age=age,
-    age_suffix=age_suffix,
-    wines=wines,
-)
+    rendered_page = template.render(
+        age=age,
+        age_suffix=age_suffix,
+        wines=wines,
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
